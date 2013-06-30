f6fb2f8a2e364aaf2e01cae3a226a406af1da751
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2013-06-30 13:21:22 +0400

    Add seorsus_sup

---
+++ src/seorsus_sup.erl
@@ -0,0 +1,24 @@
+-module(seorsus__sup).
+-behaviour(supervisor).
+
+-export([start_link/3]).
+-export([init/1]).
+
+start_link(ConnectionName, AmqpParams, ChannelsSpec) ->
+	supervisor:start_link(?MODULE, {ConnectionName, AmqpParams, ChannelsSpec}).
+
+init({ConnectionName, AmqpParams, ChannelsSpec}) ->
+	ConnectionSpec = {ConnectionName,
+		{seorsus_bridge, start_link_connection, [ConnectionName, AmqpParams]},
+		permanent,
+		2000,
+		worker,
+		[seorsus_bridge]},
+	ChannelSupSpec = {{ConnectionName, channels_sup},
+		{seorsus_channels_sup, start_link, [ConnectionName, ChannelsSpec]},
+		permanent,
+		2000,
+		supervisor,
+		[seorsus_channels_sup]},
+        {ok, {{rest_for_one,1,2}, [ConnectionSpec, ChannelSupSpec]}}.
+
