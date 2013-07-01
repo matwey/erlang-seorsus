a21d607d5966847ae89a281d14df659c79813f43
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2013-07-01 12:01:53 +0400

    Drop extra pair of [] aroung list compherension.

--- src/seorsus_channels_sup.erl
+++ src/seorsus_channels_sup.erl
@@ -8,11 +8,11 @@ start_link(ConnectionName, ChannelSpec) ->
 	supervisor:start_link(?MODULE, {ConnectionName, ChannelSpec}).
 
 init({ConnectionName, ChannelSpec}) ->
-	Spec = [[ {ChannelName,
+	Spec = [ {ChannelName,
 		{seorsus_channel_sup, start_link, [ConnectionName, ChannelName, ConsumersSpec]},
 		permanent,
 		2000,
 		supervisor,
-		[seorsus_channel_sup]} || {ChannelName, ConsumersSpec} <- ChannelSpec]],
+		[seorsus_channel_sup]} || {ChannelName, ConsumersSpec} <- ChannelSpec],
         {ok, {{one_for_one,1,2}, Spec}}.
 
